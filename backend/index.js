14304cf3fff42fa432d8801e8ebd465db41eead1
Rishu mehta <rishumehta7370@gmail.com>
2024-02-18 01:28:23 +0530

    Done with the python execution

--- backend/index.js
+++ backend/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const { generateFile } = require('./generatefile.js');
 const {executeCpp} = require('./executeCpp.js');
 const cors = require ('cors');
+const {executePy} = require('./executePy.js');
 
 
 
@@ -17,13 +18,21 @@ app.post('/run',async (req,res)=>{
     // const language =req.body.language;
     // const code= req.body.code;
 
-    const { language ="cpp", code } = req.body;
+    const { language , code } = req.body;
+    console.log(language)
     if(code==undefined){
         return res.status(400).json({success:false , error:"Empty code body "})
     }
     try{
-    const filepath = await generateFile(language ,code )
-    const output = await executeCpp(filepath)
+    const filepath = await generateFile(language ,code );
+    let output;
+    if (language==="cpp"){
+    output = await executeCpp(filepath);
+    }
+    else if(language==="python"){
+    output = await executePy(filepath);
+
+    }
     return res.json({ filepath , output});
     }
     catch (err){
